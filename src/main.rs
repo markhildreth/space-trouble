98b5638d1068c4411ee4eb995f5b62c89cc9ea4e
Mark Hildreth <mark.k.hildreth@gmail.com>
2020-03-12 17:00:34 -0400

    Added Timer using TC3. Not us level, but won't be used for that.

--- src/main.rs
+++ src/main.rs
@@ -3,54 +3,43 @@
 
 extern crate panic_halt;
 
-use cortex_m_rt::exception;
 use feather_m0 as hal;
+use hal::clock::GenericClockController;
 use hal::entry;
-use hal::pac::{CorePeripherals, Peripherals};
+use hal::pac::{interrupt, CorePeripherals, Peripherals};
 use hal::prelude::*;
-
-static mut COUNTER: u32 = 0;
+use hal::timer::TimerCounter;
 
 #[entry]
 fn main() -> ! {
     let mut core = CorePeripherals::take().unwrap();
-    let peripherals = Peripherals::take().unwrap();
+    let mut peripherals = Peripherals::take().unwrap();
+
+    let mut clocks = GenericClockController::with_internal_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.PM,
+        &mut peripherals.SYSCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+
     let mut pins = hal::Pins::new(peripherals.PORT);
     let mut red_led = pins.d13.into_open_drain_output(&mut pins.port);
     red_led.set_low().unwrap();
 
-    unsafe {
-        core.SYST.csr.write(0);
-        core.SYST.set_reload(999);
-        core.SYST.clear_current();
-        core.SYST.csr.write(0b011);
-    }
+    let gclk0 = clocks.gclk0();
+    let timer_clock = clocks.tcc2_tc3(&gclk0).unwrap();
+    let mut timer = TimerCounter::tc3_(&timer_clock, peripherals.TC3, &mut peripherals.PM);
+    timer.start(20.khz());
 
+    let mut ms = 0;
     loop {
-        if unsafe { COUNTER == 1000 } {
-            red_led.set_high().unwrap();
+        if let Ok(_) = timer.wait() {
+            ms += 1;
         }
-        if unsafe { COUNTER == 2000 } {
-            red_led.set_low().unwrap();
-        }
-        if unsafe { COUNTER == 3000 } {
-            red_led.set_high().unwrap();
-        }
-        if unsafe { COUNTER == 4000 } {
-            red_led.set_low().unwrap();
-        }
-        if unsafe { COUNTER == 5000 } {
-            red_led.set_high().unwrap();
-        }
-        if unsafe { COUNTER == 6000 } {
-            red_led.set_low().unwrap();
-        }
-    }
-}
 
-#[exception]
-fn SysTick() {
-    unsafe {
-        COUNTER += 1;
+        // 1 sec
+        if ms == 20_000 {
+            red_led.set_high().unwrap();
+        }
     }
 }
