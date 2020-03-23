3e4813b89105c423a0468a0525f0471fe1a3841a
Mark Hildreth <mark.k.hildreth@gmail.com>
2020-03-22 21:53:00 -0400

    Build script now requires device filename as parameter

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+if [ "$1" == "" ]; then
+	echo "ERROR: Must supply device file (e.g., /dev/cu.usbmodem14401"
+	echo ""
+	exit 1
+fi
+
 set -e
 cargo build --release --target="thumbv6m-none-eabi"
 arm-none-eabi-objcopy -O binary target/thumbv6m-none-eabi/release/space_trouble target/thumbv6m-none-eabi/release/space_trouble.bin
-bossac -p /dev/cu.usbmodem14401 -a -e -w -v -R --offset=0x2000 target/thumbv6m-none-eabi/release/space_trouble.bin
+bossac -p $1 -a -e -w -v -R --offset=0x2000 target/thumbv6m-none-eabi/release/space_trouble.bin
