de533c7869af8174bd97c2d44f1331a9f98b503a
Mark Hildreth <mark.k.hildreth@gmail.com>
2020-03-27 12:46:22 -0400

    Crudely patched some warnings.

--- st_client/src/game_screen.rs
+++ st_client/src/game_screen.rs
@@ -1,5 +1,4 @@
 use crate::LCD;
-use core::fmt;
 
 type StaticStrRef = &'static str;
 
@@ -93,21 +92,21 @@ impl<T: LCD> GameScreen<T> {
     pub fn update(&mut self) {
         if let DirtyState::Dirty(new) = self.distance.clean() {
             self.lcd.set_cursor_pos(0, 0);
-            self.lcd.write_fmt(format_args!("{} km", new));
+            self.lcd.write_fmt(format_args!("{} km", new)).unwrap();
         }
 
         if let DirtyState::Dirty(new) = self.hull_health.clean() {
             self.lcd.set_cursor_pos(0, 16);
-            self.lcd.write_fmt(format_args!("{: >3}%", new));
+            self.lcd.write_fmt(format_args!("{: >3}%", new)).unwrap();
         }
 
         if let DirtyState::Dirty(new) = self.timer.clean() {
             self.lcd.set_cursor_pos(1, 0);
             for _ in 0..new {
-                self.lcd.write_char(BLOCK);
+                self.lcd.write_char(BLOCK).unwrap();
             }
             for _ in new..20 {
-                self.lcd.write_char(' ');
+                self.lcd.write_char(' ').unwrap();
             }
         }
 
